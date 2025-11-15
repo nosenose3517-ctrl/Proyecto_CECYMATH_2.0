8bf9cdb79fa1959f6975fc5ab1152cb718ac16de
nosenose3517-ctrl <nosenose3517@gmail.com>
2025-11-15 20:47:57 +0000

    Leo jovany miguel lopez

--- PROYECTO_CECYMATH_2_0/ecuaciones_de_primer_grado/apps.py
+++ PROYECTO_CECYMATH_2_0/ecuaciones_de_primer_grado/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class CalculoDeAreasYPerimetrosConfig(AppConfig):
+class EcuacionesDePrimerGradoConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
-    name = "Calculo_de_areas_y_perimetros"
+    name = "ecuaciones_de_primer_grado"
---
+++ PROYECTO_CECYMATH_2_0/ecuaciones_de_segundo_grado_con_formula_general/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class EcuacionesDeSegundoGradoConFormulaGeneralConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "ecuaciones_de_segundo_grado_con_formula_general"
